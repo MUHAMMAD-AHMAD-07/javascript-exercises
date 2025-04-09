a8c568757a2a785d384a5027b0751c1e61257437
MUHAMMAD-AHMAD-07 <not.roshaan@gmail.com>
2025-04-09 16:15:11 +0500

    I completed the function to remove any number of elements from an array even if its repeated multiple times. I have to look to .splice deeply as i was facing a bug while solving.

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function(myString) {
+    let size= myString.length;
+    let reverseString= '';
 
+    for(let i= size-1;i>-1;i--){
+        reverseString=reverseString+myString[i];
+    }
+    return reverseString;
 };
 
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,19 @@
-const removeFromArray = function() {
-};
+const removeFromArray = function(originalArray, ...elementsToRemove) {
 
+
+    let removeElementsSize = elementsToRemove.length;
+
+    for(let i=0; i<removeElementsSize; i++){
+        for(let j= 0; j<originalArray.length; j++){
+            if(elementsToRemove[i] === originalArray[j]){
+                originalArray.splice(j,1);
+                j--; //Array.splice reduces the size every time it is called
+                    // so we may skip any element if we dont decrement j by 1
+                    // as j will be '1' step forward.
+            }
+        }
+    }
+    return originalArray;
+};
 // Do not edit below this line
 module.exports = removeFromArray;
