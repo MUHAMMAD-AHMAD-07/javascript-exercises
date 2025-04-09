384041d7e47daff5f2d8511b8c13e88d69567be3
MUHAMMAD-AHMAD-07 <not.roshaan@gmail.com>
2025-04-09 06:03:58 +0500

    Completed the repeat string function.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,17 @@
-const repeatString = function() {
+const repeatString = function(myString,repeatTimes) {
 
+    if(repeatTimes<0){
+        return 'ERROR';
+    }
+    
+    let finalString = '';
+    for(let i=0;i<repeatTimes;i++){
+        finalString = finalString + myString;
+    }
+    return finalString;
 };
+
+
 
 // Do not edit below this line
 module.exports = repeatString;
