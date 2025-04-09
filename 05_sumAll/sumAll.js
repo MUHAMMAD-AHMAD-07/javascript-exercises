5b10285859c74d4da7f3c5dc96fe9d5d6112b890
MUHAMMAD-AHMAD-07 <not.roshaan@gmail.com>
2025-04-09 16:48:47 +0500

    I completed the function sumAll with O(n) time complexity.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,27 @@
-const sumAll = function() {
+const sumAll = function(first,last) {
 
+    if(typeof(first)!=typeof(last) || first<0 || last<0 ||
+     !Number.isInteger(first) || !Number.isInteger(last)){
+        return 'ERROR'
+    }
+    let loopRunTimes=0;
+    if(first>last){
+        loopRunTimes = first-last;
+    }
+    else {
+        loopRunTimes = last-first;
+    }
+    let sum = first;
+    for(let i=0;i<loopRunTimes;i++){
+        if(last>first){
+            first++;
+        }
+        else{
+            first--;
+        }
+        sum+=first;
+    }
+    return sum;
 };
 
 // Do not edit below this line
