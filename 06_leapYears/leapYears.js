8b46bc4b1d216f3c69ca17fcf07f2d6ed6e68ff0
MUHAMMAD-AHMAD-07 <not.roshaan@gmail.com>
2025-04-09 17:07:24 +0500

    I completed the function leap year.

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,15 @@
-const leapYears = function() {
-
+const leapYears = function(yearToCheck) {
+    if(yearToCheck % 100 == 0){
+        if(yearToCheck % 400 == 0){ //If its divisible by 100 then it must
+            return true;           // also divisible by 400
+        }
+            return false;
+    }
+    else if( yearToCheck % 4 == 0){
+        return true;
+    }
+    else
+        return false;
 };
 
 // Do not edit below this line
